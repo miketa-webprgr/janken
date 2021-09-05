243c580881cd21ac363bf519e1b53ec105bad4c8
miketa-webprgr <ksuedomi91@gmail.com>
2021-09-06 08:32:37 +0900

    リファクタ

--- janken.rb
+++ janken.rb
@@ -1,13 +1,15 @@
+require 'byebug'
+
 GCP = { g: 'グー', c: 'チョキ', p: 'パー' }
-GCP_NUMBER = { 1 => :g, 2 => :c, 3 => :p }
 GCP_RESULT = { g: { g: 'あいこ', c: '勝ち', p: '負け' },
                c: { g: '負け', c: 'あいこ', p: '勝ち' },
                p: { g: '勝ち', c: '負け', p: 'あいこ' },
              }
 
 def janken_battle
-  initialize_battle_count
-  initialize_win_and_lose_count
+  @battle_count = set_battle_count
+  @win_count = 0
+  @lose_count = 0
 
   while @win_count + @lose_count < @battle_count do
     janken
@@ -16,58 +18,47 @@ def janken_battle
   show_janken_battle_result
 end
 
-def initialize_battle_count
-  ask_battle_count
+def set_battle_count
+  puts "何本勝負？(press 1 or 3 or 5)"
+  battle_count = gets.chomp.to_i
 
-  until [1, 3, 5].include?(@battle_count) do
+  unless [1, 3, 5].include?(battle_count)
     puts "\n1, 3, 5回勝負しか出来ません。。。"
-    ask_battle_count
+    return set_battle_count
   end
 
-  puts "#{@battle_count}本勝負を選びました"
-end
-
-def ask_battle_count
-  puts "何本勝負？(press 1 or 3 or 5)"
-  @battle_count = gets.chomp.to_i
-end
-
-def initialize_win_and_lose_count
-  @win_count = 0
-  @lose_count = 0
+  puts "#{battle_count}本勝負を選びました"
+  battle_count
 end
 
 def janken
-  store_user_hand
-  show_cpu_hand
-  show_user_hand
+  @user_hand = set_user_hand
+  @cpu_hand = set_cpu_hand
   judge_winner
 end
 
-def store_user_hand
-  ask_janken_hand
-
-  until GCP.key?(@user_hand) do
-    puts "ふざけないでください！ 正しい値を入力してください！！！"
-    ask_janken_hand
-  end
-end
-
-def ask_janken_hand
+def set_user_hand
   puts "\nじゃんけん…(press g or c or p)"
-  @user_hand = gets.chomp.to_sym
+  user_hand = gets.chomp.to_sym
+
+  until GCP.key?(user_hand) do
+    puts "ふざけないでください！ 正しい値を入力してください！！！"
+    return set_user_hand
+  end
+
+  user_hand
 end
 
-def show_cpu_hand
-  @cpu_hand = GCP_NUMBER[rand(1..3)]
-  puts "\nCPU...#{GCP[@cpu_hand]}"
-end
+def set_cpu_hand
+  gcp_key = GCP.keys.sample
 
-def show_user_hand
-  puts "あなた...#{GCP[@user_hand]}"
+  puts "\nCPU...#{GCP[gcp_key]}"
+  gcp_key
 end
 
 def judge_winner 
+  puts "あなた...#{GCP[@user_hand]}"
+
   result = GCP_RESULT[@user_hand][@cpu_hand]
   puts result + "!"
   
